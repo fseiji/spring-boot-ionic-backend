55308c0c53fb0535038546cc8a553841260297c9
Fernando Seiji <fseiji@gmail.com>
2018-06-26 17:55:10 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/fseiji/cursomc/CursomcApplication.java
+++ src/main/java/com/fseiji/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.fseiji.cursomc.domain.Cidade;
 import com.fseiji.cursomc.domain.Cliente;
 import com.fseiji.cursomc.domain.Endereco;
 import com.fseiji.cursomc.domain.Estado;
+import com.fseiji.cursomc.domain.ItemPedido;
 import com.fseiji.cursomc.domain.Pagamento;
 import com.fseiji.cursomc.domain.PagamentoComBoleto;
 import com.fseiji.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.fseiji.cursomc.repositories.CidadeRepository;
 import com.fseiji.cursomc.repositories.ClienteRepository;
 import com.fseiji.cursomc.repositories.EnderecoRepository;
 import com.fseiji.cursomc.repositories.EstadoRepository;
+import com.fseiji.cursomc.repositories.ItemPedidoRepository;
 import com.fseiji.cursomc.repositories.PagamentoRepository;
 import com.fseiji.cursomc.repositories.PedidoRepository;
 import com.fseiji.cursomc.repositories.ProdutoRepository;
@@ -46,8 +48,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	@Autowired 
+	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -96,20 +100,34 @@ public class CursomcApplication implements CommandLineRunner {
 
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
-		
+
 		Pagamento pgto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pgto1);
-		
-		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+
+		Pagamento pgto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
 		ped2.setPagamento(pgto2);
-		
-		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
-		
-		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
-		pagamentoRepository.saveAll(Arrays.asList(pgto1,pgto2));
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pgto1, pgto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p1.getItens().addAll(Arrays.asList(ip3));
+		p1.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
--- src/main/java/com/fseiji/cursomc/domain/Produto.java
+++ src/main/java/com/fseiji/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.fseiji.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -31,6 +34,8 @@ public class Produto implements Serializable {
 		inverseJoinColumns = @JoinColumn(name = "categoria_id")
 	)
 	private List<Categoria> categorias = new ArrayList<>();
+	@OneToMany(mappedBy="id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Produto() {
 	}
@@ -40,6 +45,14 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos() {
+		List<Pedido> list = new ArrayList<>();
+		for(ItemPedido itemPedido : itens) {
+			list.add(itemPedido.getPedido());
+		}
+		return list;
 	}
 
 	public Integer getId() {
@@ -74,6 +87,14 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
