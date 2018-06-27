ad612537391e102c5d4aaca0d71a1b38480ff637
Fernando Seiji <fseiji@gmail.com>
2018-06-27 10:45:58 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/fseiji/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fseiji/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,16 +7,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.fseiji.cursomc.services.exceptions.DataIntegrityException;
 import com.fseiji.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-		StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), 
-				e.getMessage(), System.currentTimeMillis());
+		StandardError standardError = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(),
+				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standardError);
+	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+		StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
 	}
 
 }
--- src/main/java/com/fseiji/cursomc/services/CategoriaService.java
+++ src/main/java/com/fseiji/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.fseiji.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.fseiji.cursomc.domain.Categoria;
 import com.fseiji.cursomc.repositories.CategoriaRepository;
+import com.fseiji.cursomc.services.exceptions.DataIntegrityException;
 import com.fseiji.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,21 +16,29 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " +id+ ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria categoria) {
 		categoria.setId(null);
 		return categoriaRepository.save(categoria);
 	}
-	
+
 	public Categoria update(Categoria categoria) {
 		find(categoria.getId());
 		return categoriaRepository.save(categoria);
 	}
-	
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);			
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
+	}
 }
