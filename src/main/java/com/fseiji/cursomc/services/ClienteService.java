c454795bbd58655f61dbf47fa6b84017462586ce
Fernando Seiji <fseiji@gmail.com>
2018-06-28 18:22:36 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/fseiji/cursomc/services/ClienteService.java
+++ src/main/java/com/fseiji/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.fseiji.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.fseiji.cursomc.domain.Cidade;
 import com.fseiji.cursomc.domain.Cliente;
+import com.fseiji.cursomc.domain.Endereco;
+import com.fseiji.cursomc.domain.enums.TipoCliente;
 import com.fseiji.cursomc.dto.ClienteDTO;
+import com.fseiji.cursomc.dto.ClienteNewDTO;
 import com.fseiji.cursomc.repositories.ClienteRepository;
+import com.fseiji.cursomc.repositories.EnderecoRepository;
 import com.fseiji.cursomc.services.exceptions.DataIntegrityException;
 import com.fseiji.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,12 +29,23 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public Cliente find(Integer id) {
 		Optional<Cliente> cliente = clienteRepository.findById(id);
-		return cliente.orElseThrow(() -> new ObjectNotFoundException((
-				"Objeto não encontrado! Id: " +id+ ", Tipo: " + Cliente.class.getName())));
+		return cliente.orElseThrow(() -> new ObjectNotFoundException(
+				("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())));
 	}
-	
+
+	@Transactional
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		cliente = clienteRepository.save(cliente);
+		enderecoRepository.saveAll(cliente.getEnderecos());
+		return cliente;
+	}
+
 	public Cliente update(Cliente cliente) {
 		Cliente newCliente = find(cliente.getId());
 		updateData(newCliente, cliente);
@@ -51,11 +69,28 @@ public class ClienteService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return clienteRepository.findAll(pageRequest);
 	}
-	
+
 	public Cliente fromCliente(ClienteDTO clienteDTO) {
 		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromCliente(ClienteNewDTO clienteNewDTO) {
+		Cliente cliente = new Cliente(null, clienteNewDTO.getNome(), clienteNewDTO.getEmail(),
+				clienteNewDTO.getCpfOuCnpj(), TipoCliente.toEnum(clienteNewDTO.getTipoCliente()));
+		Cidade cidade = new Cidade(clienteNewDTO.getCidadeId(), null, null);
+		Endereco endereco = new Endereco(null, clienteNewDTO.getLogradouro(), clienteNewDTO.getNumero(),
+				clienteNewDTO.getComplemento(), clienteNewDTO.getBairro(), clienteNewDTO.getCep(), cliente, cidade);
+		cliente.getEnderecos().add(endereco);
+		cliente.getTelefones().add(clienteNewDTO.getTelefone());
+		if(clienteNewDTO.getTelefone1()!=null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone1());
+		}
+		if(clienteNewDTO.getTelefone2()!=null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone2());
+		}
+		return cliente;
+	}
+
 	private void updateData(Cliente newCliente, Cliente cliente) {
 		newCliente.setNome(cliente.getNome());
 		newCliente.setEmail(cliente.getEmail());
