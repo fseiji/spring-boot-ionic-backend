28616115a01aed3149bb2905bbac73913dc07270
Fernando Seiji <fseiji@gmail.com>
2018-06-28 14:04:00 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/fseiji/cursomc/services/CategoriaService.java
+++ src/main/java/com/fseiji/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fseiji.cursomc.domain.Categoria;
+import com.fseiji.cursomc.dto.CategoriaDTO;
 import com.fseiji.cursomc.repositories.CategoriaRepository;
 import com.fseiji.cursomc.services.exceptions.DataIntegrityException;
 import com.fseiji.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return categoriaRepository.findAll(pageRequest);
+	}
+	
+	public Categoria fromCategoria(CategoriaDTO categoriaDTO) {
+		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
 	}
 }
